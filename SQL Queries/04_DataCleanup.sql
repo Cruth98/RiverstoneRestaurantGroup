2d934f96e7377f402fca518aba2c3a7dfd6c4b0b
Cruth98 <43446644+Cruth98@users.noreply.github.com>
2026-03-10 18:06:11 -0400

    Update orders concept_id to match restaurant concept_id
    Aligns orders concept_id with the corresponding restaurant concept_id.

--- SQL Queries/04_DataCleanup.sql
+++ SQL Queries/04_DataCleanup.sql
@@ -15,3 +15,12 @@ INNER JOIN dbo.menu_categories mc
     ON mi.menu_category_id = mc.menu_category_id
 WHERE mi.concept_id <> mc.concept_id;
 GO
+
+/* Align orders concept_id to restaurant concept_id */
+UPDATE o
+SET o.concept_id = r.concept_id
+FROM dbo.orders o
+INNER JOIN dbo.restaurants r
+    ON o.restaurant_id = r.restaurant_id
+WHERE o.concept_id <> r.concept_id;
+GO
